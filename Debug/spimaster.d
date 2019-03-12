15d2a11c97e9b72a411194e98f317348143b37f7
Keenan Wulff <keenan.wulff@outlook.com>
2019-03-11 20:32:05 -0600

    first commit

---
+++ Debug/spimaster.d
@@ -0,0 +1,99 @@
+# FIXED
+
+spimaster.obj: ../spimaster.c
+spimaster.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdbool.h
+spimaster.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stddef.h
+spimaster.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdint.h
+spimaster.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/stdint.h
+spimaster.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/cdefs.h
+spimaster.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_types.h
+spimaster.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_types.h
+spimaster.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_stdint.h
+spimaster.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_stdint.h
+spimaster.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/string.h
+spimaster.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/_ti_config.h
+spimaster.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/linkage.h
+spimaster.obj: C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/posix/ccs/pthread.h
+spimaster.obj: C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/posix/ccs/sys/types.h
+spimaster.obj: C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/posix/ccs/sys/_internal.h
+spimaster.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/../include/time.h
+spimaster.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/abi_prefix.h
+spimaster.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/xlocale/_time.h
+spimaster.obj: C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/posix/ccs/time.h
+spimaster.obj: C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/posix/ccs/signal.h
+spimaster.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/../include/signal.h
+spimaster.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/../include/sys/timespec.h
+spimaster.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_timespec.h
+spimaster.obj: C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/posix/ccs/sched.h
+spimaster.obj: C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/posix/ccs/semaphore.h
+spimaster.obj: C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/posix/ccs/unistd.h
+spimaster.obj: C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/drivers/GPIO.h
+spimaster.obj: C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/drivers/SPI.h
+spimaster.obj: C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/display/Display.h
+spimaster.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdarg.h
+spimaster.obj: ../Board.h
+spimaster.obj: ../CC1350_BIOSENSE_HEADSET.h
+spimaster.obj: C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/drivers/PIN.h
+spimaster.obj: C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/devices/cc13x0/driverlib/ioc.h
+spimaster.obj: C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_types.h
+spimaster.obj: C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/devices/cc13x0/driverlib/../inc/../inc/hw_chip_def.h
+spimaster.obj: C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_memmap.h
+spimaster.obj: C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_ioc.h
+spimaster.obj: C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_ints.h
+spimaster.obj: C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/devices/cc13x0/driverlib/interrupt.h
+spimaster.obj: C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_nvic.h
+spimaster.obj: C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/devices/cc13x0/driverlib/debug.h
+spimaster.obj: C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/devices/cc13x0/driverlib/cpu.h
+spimaster.obj: C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_cpu_scs.h
+spimaster.obj: C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/devices/cc13x0/driverlib/../driverlib/rom.h
+spimaster.obj: C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/devices/cc13x0/driverlib/gpio.h
+spimaster.obj: C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_gpio.h
+
+../spimaster.c: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdbool.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stddef.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/cdefs.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/string.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/_ti_config.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/linkage.h: 
+C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/posix/ccs/pthread.h: 
+C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/posix/ccs/sys/types.h: 
+C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/posix/ccs/sys/_internal.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/../include/time.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/abi_prefix.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/xlocale/_time.h: 
+C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/posix/ccs/time.h: 
+C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/posix/ccs/signal.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/../include/signal.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/../include/sys/timespec.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_timespec.h: 
+C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/posix/ccs/sched.h: 
+C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/posix/ccs/semaphore.h: 
+C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/posix/ccs/unistd.h: 
+C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/drivers/GPIO.h: 
+C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/drivers/SPI.h: 
+C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/display/Display.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdarg.h: 
+../Board.h: 
+../CC1350_BIOSENSE_HEADSET.h: 
+C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/drivers/PIN.h: 
+C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/devices/cc13x0/driverlib/ioc.h: 
+C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_types.h: 
+C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/devices/cc13x0/driverlib/../inc/../inc/hw_chip_def.h: 
+C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_memmap.h: 
+C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_ioc.h: 
+C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_ints.h: 
+C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/devices/cc13x0/driverlib/interrupt.h: 
+C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_nvic.h: 
+C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/devices/cc13x0/driverlib/debug.h: 
+C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/devices/cc13x0/driverlib/cpu.h: 
+C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_cpu_scs.h: 
+C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/devices/cc13x0/driverlib/../driverlib/rom.h: 
+C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/devices/cc13x0/driverlib/gpio.h: 
+C:/ti/simplelink_cc13x0_sdk_2_40_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_gpio.h: 
---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,53 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../CC1350_LAUNCHXL_TIRTOS.cmd 
+
+C_SRCS += \
+../CC1350_BIOSENSE_HEADSET.c \
+../CC1350_LAUNCHXL_fxns.c \
+../ccfg.c \
+../main_tirtos.c \
+../spimaster.c 
+
+C_DEPS += \
+./CC1350_BIOSENSE_HEADSET.d \
+./CC1350_LAUNCHXL_fxns.d \
+./ccfg.d \
+./main_tirtos.d \
+./spimaster.d 
+
+OBJS += \
+./CC1350_BIOSENSE_HEADSET.obj \
+./CC1350_LAUNCHXL_fxns.obj \
+./ccfg.obj \
+./main_tirtos.obj \
+./spimaster.obj 
+
+OBJS__QUOTED += \
+"CC1350_BIOSENSE_HEADSET.obj" \
+"CC1350_LAUNCHXL_fxns.obj" \
+"ccfg.obj" \
+"main_tirtos.obj" \
+"spimaster.obj" 
+
+C_DEPS__QUOTED += \
+"CC1350_BIOSENSE_HEADSET.d" \
+"CC1350_LAUNCHXL_fxns.d" \
+"ccfg.d" \
+"main_tirtos.d" \
+"spimaster.d" 
+
+C_SRCS__QUOTED += \
+"../CC1350_BIOSENSE_HEADSET.c" \
+"../CC1350_LAUNCHXL_fxns.c" \
+"../ccfg.c" \
+"../main_tirtos.c" \
+"../spimaster.c" 
+
+
